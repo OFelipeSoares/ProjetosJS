8b11e2cb5e36e99489104a64eb9655a09117983f
OFelipeSoares <felipebusiness.soares@gmail.com>
2022-12-06 21:44:23 -0300

    Upload primeira parte

---
+++ Modelo/script.js
@@ -0,0 +1,8 @@
+let num = document.getElementById('txtn')
+let lista = document.querySelector('selector#flista')
+let res = document.querySelector('div#res')
+let valores = []
+function adicionar(){
+    if(isNumero(num.value) && !inLista(num.value))
+}
+return num
